9b90786d2083d47bdd67ddb1f5bf1ca2f76a55a2
Josue Matailo <jdmatailo@sudamericano.edu.ec>
2022-11-23 10:02:38 -0500

    prueba

---
+++ src/main/kotlin/com/example/facturasapi/controller/ProductController.kt
@@ -0,0 +1,37 @@
+package com.example.facturasapi.controller
+
+import com.example.facturasapi.model.Client
+import com.example.facturasapi.service.ClientService
+import org.hibernate.annotations.UpdateTimestamp
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
+
+@RestController
+@RequestMapping("/client")
+class ClientController {
+
+    @Autowired
+    lateinit var  clientService: ClientService
+
+    @GetMapping
+    fun list():List<Client>{
+        return clientService.list()
+    }
+    @PostMapping
+    fun save(@RequestBody @Valid client:Client):ResponseEntity<Client>{
+        return ResponseEntity(clientService.save(client), HttpStatus.OK)
+    }
+
+    @PutMapping
+    fun update (@RequestBody client:Client):ResponseEntity<Client>{
+        return ResponseEntity(clientService.update(client), HttpStatus.OK)
+    }
+    @PatchMapping
+    fun updateName (@RequestBody client:Client):ResponseEntity<Client>{
+        return ResponseEntity(clientService.updateName(client), HttpStatus.OK)
+    }
+
+}
---
+++ src/main/kotlin/com/example/facturasapi/service/DetailService.kt
@@ -0,0 +1,56 @@
+package com.example.facturasapi.service
+
+import com.example.facturasapi.model.Client
+import com.example.facturasapi.repository.ClientRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
+
+@Service
+class ClientService {
+
+    @Autowired
+    lateinit var clientRepository: ClientRepository
+
+    fun list():List<Client>{
+        return clientRepository.findAll()
+    }
+
+
+    fun save(client: Client):Client{
+        return clientRepository.save(client)
+    }
+
+    fun update(client:Client): Client {
+        try{
+            clientRepository.findByid(client.id)
+                ?: throw Exception("ID no existe")
+
+            return clientRepository.save(client)
+        }
+        catch (ex:Exception){
+            throw ResponseStatusException(HttpStatus.NOT_FOUND,ex.message)
+        }
+    }
+
+
+    fun updateName(client:Client): Client {
+        try{
+            val response = clientRepository.findByid(client.id)
+                ?: throw Exception("ID no existe")
+            response.apply {
+                fullname =client.fullname
+            }
+            return clientRepository.save(response)
+        }
+        catch (ex:Exception){
+            throw ResponseStatusException(HttpStatus.NOT_FOUND,ex.message)
+        }
+    }
+
+}
+
+
+
+
